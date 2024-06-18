64d31d59a39828500c3d9249e0b30dc93353809a
qxieyong <143081419+qxieyong@users.noreply.github.com>
2024-06-18 18:20:14 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import "@/utils/autoUpdate";
 import "@/utils/adaptation";
 
 import { Provider } from "react-redux";
