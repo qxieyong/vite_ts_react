c12eebfa88d7e764caf02436e226c886d1fc0f2f
qxieyong <143081419+qxieyong@users.noreply.github.com>
2024-06-18 18:19:02 +0800

    Update autoUpdate.ts

--- src/utils/autoUpdate.ts
+++ src/utils/autoUpdate.ts
@@ -1,56 +1,27 @@
-const data = localStorage.getItem('LastScrs');
-let lastScrs = data ? JSON.parse(data) : ""; //上一次获取的script地址
+const autoUpdate = () => {
+    fetch('/')
+        .then(response => response.text())
+        .then(data => {
+            const dom = new DOMParser().parseFromString(data, 'text/html');
+            const scripts = dom.querySelectorAll('script[type="module"]') as unknown as HTMLScriptElement[];
+            console.log(scripts, 'bbb')
+            scripts.forEach(script => {
+                console.log(script, '标签');
+                if (script.src.includes(window.location.origin)) {
+                    const currentVersion = window.localStorage.getItem('version');
+                    if (currentVersion !== script.src) {
+                        console.log('版本不一样', currentVersion, script.src);
+                        window.localStorage.setItem('version', script.src);
+                        setTimeout(() => {
+                            location.reload();
+                        }, 1000);
+                    }
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+};
 
-// const scriptReg = /\<script.*src=["'](?<src>[^"']+)/gm;
-
-// 获取最新页面中的src连接
-
-async function extractNewScripts() {
-	const html = await fetch("/?_timestamp=" + Date.now()).then((res) => res.text());
-	const dom = document.createElement('div');
-	dom.innerHTML = html;
-	// scriptReg.lastIndex = 0;
-	const result = [''];
-	[...dom.children].forEach(val => {
-		if (val.tagName === 'SCRIPT' && (val as HTMLScriptElement).src) {
-			result.push((val as HTMLScriptElement).src);
-		}
-	})
-
-	return result;
-}
-
-async function needUpdate() {
-	const newScripts = await extractNewScripts();
-	if (!lastScrs) {
-		lastScrs = newScripts;
-		localStorage.setItem('LastScrs', JSON.stringify(lastScrs));
-		return false;
-	}
-	let result = false;
-	if (lastScrs.length !== newScripts.length) {
-		result = true;
-	}
-	for (let i = 0; i < lastScrs.length; i++) {
-		if (lastScrs[i] !== newScripts[i]) {
-			result = true;
-			break;
-		}
-	}
-	lastScrs = newScripts;
-	localStorage.setItem('LastScrs', JSON.stringify(lastScrs));
-	return result;
-}
-
-// const DURATION = 2000;
-async function autoRefresh() {
-	const willUpdate = await needUpdate();
-	if (willUpdate) {
-		// const result = confirm("更新页面");
-		// if (result) {
-		location.reload();
-		// }
-	}
-}
-
-autoRefresh();
+export default autoUpdate;
