685ad965c3322221a196d3b9315085c799c57aa7
qxieyong <143081419+qxieyong@users.noreply.github.com>
2024-06-18 16:11:29 +0800

    Update adaptation.ts

--- src/utils/adaptation.ts
+++ src/utils/adaptation.ts
@@ -1,19 +1,26 @@
 //适配兼容
+import debounce from './debounce';
 (function (doc, win) {
-    console.log('win.devicePixelRatio', win.devicePixelRatio)
-    // const dpr = Math.min(win.devicePixelRatio, 3);
-    // const scale = 1 / dpr;
-    const resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize';   //orientationchange 屏幕方向变化
+    console.log('win.devicePixelRatio', win.devicePixelRatio);
+    const dpr = Math.min(win.devicePixelRatio, 3),
+        scale = 1 / dpr,
+        resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize';
 
     const recalCulate = function () {
-        const docEle = document.documentElement;
-        const w = docEle.clientWidth;
-        const num = (w > 1920 ? 1920 : w) / 1920;       // **此时的1920就是你设计稿的尺寸
-        let fontSize = num * 100;// * scale;
-        fontSize = Math.max(fontSize, 24)
-        docEle.style.fontSize = fontSize.toFixed(1) + 'px';
+        let docEle = document.documentElement,
+            w = docEle.clientWidth,
+            num = Math.ceil(w / 1920 * 100);  // **此时的1920就是你设计稿的尺寸
+            // num = w /  100;  // **此时的1920就是你设计稿的尺寸
+        let fontSize = num > 100 ? 100 : num < 30 ? 30 : num;
+
+        docEle.style.fontSize = fontSize + 'px';
     };
+
+    
+
+    const debouncedRecalculate = debounce(recalCulate, 100);
+
     recalCulate();
     if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvent, recalCulate, false);
+    win.addEventListener(resizeEvent, debouncedRecalculate, false);
 })(document, window);
